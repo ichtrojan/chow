59acbdb27f60c95a27865e5580647ed8a5c85f9f
Bolaji Olajide <cooproton@gmail.com>
2019-11-22 00:26:44 +0100

    feat: refactor chow library
    - add tests

--- chow/chow_test.go
+++ chow/chow_test.go
@@ -1,6 +1,12 @@
 package chow
 
-import "testing"
+import (
+	"flag"
+	"io/ioutil"
+	"testing"
+
+	"github.com/urfave/cli"
+)
 
 func TestDummy(t *testing.T) {
 	total := 5 + 5
@@ -9,3 +15,35 @@ func TestDummy(t *testing.T) {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
 	}
 }
+
+func TestChowWithEmptyPath(t *testing.T) {
+	testArgs := []string{""}
+	app := &cli.App{Writer: ioutil.Discard}
+	set := flag.NewFlagSet("test", 0)
+	_ = set.Parse(testArgs)
+
+	context := cli.NewContext(app, set, nil)
+
+	err := HandleFunc(context)
+
+	errorMessage := "No file specified: Please include a valid file"
+	if err.Error() != errorMessage {
+		t.Error("Path should be empty.")
+	}
+}
+
+// func TestChowWithBadJson(t *testing.T) {
+// 	testArgs := []string{"fixtures/fake.json"}
+// 	app := &cli.App{Writer: ioutil.Discard}
+// 	set := flag.NewFlagSet("test", 0)
+// 	_ = set.Parse(testArgs)
+
+// 	context := cli.NewContext(app, set, nil)
+
+// 	err := HandleFunc(context)
+
+// 	errorMessage := "No file specified: Please include a valid file"
+// 	if err.Error() != errorMessage {
+// 		t.Error("Path should be empty.")
+// 	}
+// }
